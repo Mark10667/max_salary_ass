74f852538fac7dd509734d0a7b799c0ca84cb190
尹秀晨 <yinxiuchen@MacBook-Pro-6.local>
2019-02-17 12:20:11 -0500

    a

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
-CC = gcc 
-CFLAGS = -Wall -std=c99 
+CC = gcc
+CFLAGS = -Wall -std=c99
 LIBS += -lm
 
 all:test
 
-max_salary_naive.o: max_salary_naive.c max_salary.h 
-	$(CC) $(CFLAGS) -c max_salary_naive.c $(LIBS)
+max_salary_naive.o: max_salary_naive.c max_salary.h
+	$(CC) $(CFLAGS) -c max_salary_naive.c
 
-max_salary_greedy.o: max_salary_greedy.c max_salary.h 
-	$(CC) $(CFLAGS) -c max_salary_greedy.c $(LIBS)
+max_salary_greedy.o: max_salary_greedy.c max_salary.h
+	$(CC) $(CFLAGS) -c max_salary_greedy.c
 
 test_performance.o: test_performance.c max_salary.h
-	$(CC) $(CFLAGS) -c test_performance.c $(LIBS)
+	$(CC) $(CFLAGS) -c test_performance.c
 
 test: max_salary_naive.o max_salary_greedy.o test_performance.o
 	$(CC) $(CFLAGS) -o test max_salary_naive.o max_salary_greedy.o test_performance.o $(LIBS)
--- test_performance.c
+++ test_performance.c
@@ -11,6 +11,8 @@ void stress_test(int N, int M){
   srand(time(NULL));   // Initialization, should only be called once.
   int* test_arr = malloc(sizeof(int) * (N+1));
   test_arr[N] = '\0';
+  digit_arr = malloc(sizeof(int)*(N+1));
+  a = malloc(sizeof(int)*(N+1));
   int greedy_max;
   while(1){
     for(int i = 0; i < N; i++){
@@ -33,61 +35,58 @@ void stress_test(int N, int M){
 }
 int main(int argc, char **argv) {
 
+    // int b[] = {19, 20, 17, 15, 1, };
+    // int n = sizeof b/sizeof b[0];
+    // digit_arr = malloc(sizeof(int)*(n+1));
+    // a = malloc(sizeof(int)*(n+1));
+    // max_salary_naive(b, n, n);
+    // printf("Naive shows max salary is %d\n", naive_max);
+    //
+    // int greedy_max = max_salary_greedy(b, n);
+    // printf("greedy_max shows max salary is %d\n", greedy_max);
+    //
+    // if(naive_max == greedy_max){
+    //   puts("Wow! they are equal!!");
+    // }
+    // free(a);
+    // free(digit_arr);
 
 
-    int b[] = {19, 20, 17, 15, 1, };
-    int n = sizeof b/sizeof b[0];
-    digit_arr = malloc(sizeof(int)*(n+1));
-    a = malloc(sizeof(int)*(n+1));
-    max_salary_naive(b, n, n);
-    printf("Naive shows max salary is %d\n", naive_max);
-
-    int greedy_max = max_salary_greedy(b, n);
-    printf("greedy_max shows max salary is %d\n", greedy_max);
-
-    if(naive_max == greedy_max){
-      puts("Wow! they are equal!!");
+    if(argc != 3){
+      puts("To run: ./test <N> <M>  (N is the number of elements in the array, M is max number of digits for one element. \n)");
+      free(digit_arr);
+      free(a);
+      return 0;
     }
-    free(a);
-    free(digit_arr);
-    // free(digit_arr);
-    // free(a);
 
-    // if(argc != 3){
-    //   puts("To run: ./test <N> <M>  (N is the number of elements in the array, M is max number of digits for one element. \n)");
-    //   free(digit_arr);
-    //   free(a);
-    //   return 0;
-    // }
-    //
-    // int N = atoi(argv[1]);
-    // int M = atoi(argv[2]);
-    //
-    // if(N < 0 || M < 0){
-    //   puts("N and M are positive, no one wants a negative salary :)\n");
-    //   free(digit_arr);
-    //   free(a);
-    //   return 0;
-    // }
-    //
-    // int b = M;
-    // int digit = 0;
-    //
-    // while(b > 0){
-    //   b = b / 10;
-    //   digit++;
-    // }
-    //
-    // if(N*digit > 9){
-    //   puts("This number is too big, please choose a smaller N and M next time :)\n");
-    //   free(digit_arr);
-    //   free(a);
-    //   return 0;
-    // }
-    //
-    // stress_test(N, M);
-    //
+    int N = atoi(argv[1]);
+    int M = atoi(argv[2]);
+
+    if(N < 0 || M < 0){
+      puts("N and M are positive, no one wants a negative salary :)\n");
+      // free(digit_arr);
+      // free(a);
+      return 0;
+    }
+
+    int b = M;
+    int digit = 0;
+
+    while(b > 0){
+      b = b / 10;
+      digit++;
+    }
+
+    if(N*digit > 9){
+      puts("This number is too big, please choose a smaller N and M next time :)\n");
+      // free(digit_arr);
+      // free(a);
+      return 0;
+    }
+
+    stress_test(N, M);
+
     // free(digit_arr);
     // free(a);
-    // return 0;
+    return 0;
 }
